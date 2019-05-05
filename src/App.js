58c4a8eff4d0128cb9990483dda203ebaac60f38
Correia Alves Cedric <cedric.correia.alves@gmail.com>
2019-05-06 00:08:38 +0200

    add ambient light api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import { hot } from "react-hot-loader/root";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles/App.css";
 import { Dizel } from "./Dizel";
 
-const App = () => (
-    <div className="App">
-        <Dizel />
-    </div>
-);
+const App = () => {
+    const [bgColor, setBgColor] = useState(255);
+
+    useEffect(() => {
+        console.log("effect ran");
+        if (window.AmbientLightSensor) {
+            const sensor = new AmbientLightSensor();
+
+            sensor.addEventListener("activate", () => {
+                console.log("Ready to report readings");
+            });
+            sensor.addEventListener("error", event => {
+                console.error(event.error.message);
+            });
+            sensor.addEventListener("reading", () => {
+                const illuminance = sensor.illuminance;
+                console.log(illuminance);
+                if (illuminance < 50) {
+                    setBgColor(0);
+                } else {
+                    setBgColor(255);
+                }
+            });
+            sensor.onreading = () => {
+                console.log(sensor.illuminance);
+            };
+            sensor.start();
+        }
+    }, []);
+    return (
+        <div
+            className="App"
+            style={{
+                backgroundColor: `rgb(${bgColor},${bgColor},${bgColor})`
+            }}
+        >
+            <Dizel />
+        </div>
+    );
+};
 
 export default hot(App);
